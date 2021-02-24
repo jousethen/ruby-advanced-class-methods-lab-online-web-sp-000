e1bbd1da44d15edb8fde6e3b3d8420f59ff2d089
Jouse then <jthen93@gmail.com>
2021-02-24 14:11:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,46 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    new_song = Song.new()
+    @@all << new_song
+    return new_song
+  end
+  
+  def self.new_by_name (name)
+    new_song = Song.new
+    @name = name
+    new_song.name = name
+    
+    return new_song
+  end
+  
+  def self.create_by_name(name)
+    new_song = Song.new
+    @name = name
+    new_song.name = name
+    @@all << new_song
+    return new_song
+  end
+  
+  def self.find_by_name(name)
+    self.all.each do |song|
+      if song.name == name
+        return song
+      end
+    end
+    return false
+  end
+  
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    
+    if song == false
+      song = self.create_by_name(name)
+    end
+    
+    return song
+      
+  end
 end
