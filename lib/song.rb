694771e98eadae986e788db6e5d2c1a288e97ade
Jouse then <jthen93@gmail.com>
2021-02-24 14:43:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -69,7 +69,7 @@ class Song
     return new_song
   end
   
-  def self.create_by_filename (filename)
+  def self.create_from_filename (filename)
     @all << self.new_from_filename(filename)
   end
 end
